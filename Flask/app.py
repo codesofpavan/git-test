9a67582fd6ed291b0afac43042998688c1d12b92
codesofpavan <103850593+codesofpavan@users.noreply.github.com>
2024-06-03 05:20:14 +0000

    push

--- Flask/app.py
+++ Flask/app.py
@@ -3,7 +3,10 @@ app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome'
+    return 'Welcome oooo'
 
+@app.route('/members')
+def members():
+    return 'Welcome to Members only Live'
 if __name__=='__main__':
-    app.run() 
+    app.run(debug=True) 
